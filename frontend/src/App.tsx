3ea97860908705177838178896933a3992c5408b
Bennet <bennet.schlotfeldt@gmx.de>
2022-02-25 18:39:45 +0100

    lauffähig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import ToDoApp from "./ToDoApp";
+import './App.css'
 
 function App() {
-
-    const [greeting, setGreeting] = useState('')
-
-    useEffect(() => {
-        fetch('/api/greeting', {
-            method: 'GET',
-            headers: {
-                'Accept': 'text/plain'
-            }
-        })
-            .then(response => response.text())
-            .then(text => setGreeting(text))
-            .catch(err => setGreeting('Da ist etwas schief gelaufen'));
-    }, []);
-
     return (
         <div>
-            {greeting}
+            <h2>Meine To-Dos</h2>
+            <div>
+
+            <ToDoApp/>
+                </div>
         </div>
     );
 }
---
+++ frontend/src/model.ts
@@ -0,0 +1,8 @@
+export interface ItemStructure{
+    formattedEndDate: string;
+    name: string;
+    description: string;
+    id: string;
+    status: boolean;
+
+}
