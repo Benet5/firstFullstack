eedcd23c3a9a4e88787c9a6533aadae025ea8fc6
Bennet <bennet.schlotfeldt@gmx.de>
2022-03-03 18:30:15 +0100

    bilingual

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import ToDoApp from "./ToDoApp";
 import './App.css'
-
+import { useTranslation } from "react-i18next";
 function App() {
+    const { t } = useTranslation();
     return (
         <div>
-            <h2>Meine To-Dos</h2>
+            <h2>{t('title')}</h2>
             <div>
 
             <ToDoApp/>
