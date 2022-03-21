29991aff7a6ee314a3a6ebbe7990ddcf7e0194c5
Bennet <bennet.schlotfeldt@gmx.de>
2022-03-21 19:07:47 +0100

    erneut

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -19,13 +19,13 @@ ReactDOM.render(
         <BrowserRouter>
             <AuthProvider>
             <Routes>
-                <Route path="/" element={<App/>}>
+                <Route path="/" element={<Login/>}>
                    <Route path="/todoapp" element={<ToDoApp/>}/>
                     <Route path="/todoapp/:itemId" element={<EditItem/>}/>
                     <Route path="/todoapp/auth" element={<Register/>}/>
                     <Route path="/todoapp/auth/login" element={<Login/>}/>
                     <Route path="/todoapp/auth/logout" element={<Logout/>}/>
-                    <Route path="*" element={<ToDoApp/>}/>
+                    <Route path="/*" element={<ToDoApp/>}/>
                 </Route>
             </Routes>
             </AuthProvider>
